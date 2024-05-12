7ace9501f710af503705947d89ba3d1b815d9cc6
Bil Bas <bil.bas@nexhealth.com>
2024-05-12 16:57:11 +0100

    Generate player cards too.

--- ecogame/player_cards.py
+++ ecogame/player_cards.py
@@ -6,15 +6,16 @@ from ecogame.base_cards import BaseCards
 
 
 class PlayerCards(BaseCards):
-    CARD_WIDTH, CARD_HEIGHT = mm_to_px(70), mm_to_px(120)
-    MARGIN_LEFT, MARGIN_RIGHT = mm_to_px(14), mm_to_px(14)
-    MARGIN_TOP, MARGIN_BOTTOM = mm_to_px(10), mm_to_px(10)
+    CARD_WIDTH, CARD_HEIGHT = mm_to_px(88.9), mm_to_px(63.5 * 2)
+    MARGIN_LEFT, MARGIN_RIGHT = mm_to_px(7), mm_to_px(7)
+    MARGIN_TOP, MARGIN_BOTTOM = mm_to_px(8), mm_to_px(8)
     INNER_WIDTH = CARD_WIDTH - MARGIN_LEFT - MARGIN_RIGHT
     INNER_HEIGHT = CARD_HEIGHT - MARGIN_TOP - MARGIN_BOTTOM
-    TITLE_Y = mm_to_px(60)
-    VALUES_Y = mm_to_px(100)
-    IMAGE_SIZE = INNER_WIDTH, INNER_WIDTH
+    NAME_Y = mm_to_px(70)
+    VALUES_Y = mm_to_px(80)
+    IMAGE_SIZE = mm_to_px(57), mm_to_px(57)
     COST_ICON_SIZE = 22, 22
+    VALUE_MARGIN = mm_to_px(12)
 
     COLS, ROWS = 2, 2
     CONFIG_FILE = "./player_cards.yaml"
@@ -24,7 +25,7 @@ class PlayerCards(BaseCards):
             yield self._card(show_border=show_border, **card_config)
 
     def _card(self, show_border: bool, name: str, image: str,
-              initial: hash, income: hash, flavour: str, products: str, pollutions: str):
+              initial: hash, income: hash, flavour: str):
 
         card = Image.new("RGBA", (self.CARD_WIDTH, self.CARD_HEIGHT), self.BACKGROUND_COLOR)
         draw = ImageDraw.Draw(card)
@@ -33,30 +34,39 @@ class PlayerCards(BaseCards):
         sized_image = self._images[image].resize(self.IMAGE_SIZE, Image.Resampling.LANCZOS)
         card.paste(sized_image, ((self.CARD_WIDTH - self.IMAGE_SIZE[0]) // 2, self.MARGIN_TOP), mask=sized_image)
 
-        # Initial prosperity and pollution
-        prosperity_icon = self._images["prosperity"].resize(self.COST_ICON_SIZE, Image.Resampling.LANCZOS)
-        card.paste(prosperity_icon, (self.MARGIN_LEFT + 15, self.MARGIN_TOP), mask=prosperity_icon)
-        pollution_icon = self._images["pollution"].resize(self.COST_ICON_SIZE, Image.Resampling.LANCZOS)
-        card.paste(pollution_icon, (self.MARGIN_LEFT + 15, self.MARGIN_TOP), mask=pollution_icon)
-
+        # Initial prosperity
         self._font.text(draw, (self.MARGIN_LEFT, self.MARGIN_TOP), str(initial["prosperity"]), color=self.INK_COLOR,
                         size=self.FONT_HEIGHT_COST)
-        self._font.text(draw, (self.MARGIN_LEFT, self.MARGIN_TOP), str(initial["pollution"]), color=self.INK_COLOR,
-                        size=self.FONT_HEIGHT_COST)
+        prosperity_icon = self._images["prosperity"].resize(self.COST_ICON_SIZE, Image.Resampling.LANCZOS)
+        card.paste(prosperity_icon, (self.MARGIN_LEFT + 15, self.MARGIN_TOP), mask=prosperity_icon)
+
+        # Initial pollution
+        self._font.text(draw, (self.CARD_WIDTH - self.MARGIN_RIGHT - 25, self.MARGIN_TOP), str(initial["pollution"]),
+                        color=self.INK_COLOR, size=self.FONT_HEIGHT_COST, anchor="ra")
+        pollution_icon = self._images["pollution"].resize(self.COST_ICON_SIZE, Image.Resampling.LANCZOS)
+        card.paste(pollution_icon, (self.CARD_WIDTH - self.MARGIN_RIGHT - self.COST_ICON_SIZE[0], self.MARGIN_TOP),
+                   mask=pollution_icon)
 
         # Name
-        self._font.text(draw, (self.CARD_WIDTH // 2, self.TITLE_Y), name, color=self.INK_COLOR, size=self.FONT_HEIGHT_TITLE,
-                        anchor="ma")
+        self._font.text(draw, (self.CARD_WIDTH // 2, self.NAME_Y), name, color=self.INK_COLOR,
+                        size=self.FONT_HEIGHT_TITLE, anchor="ma")
 
-        # Production and Pollution
-        self._font.text(draw, (self.MARGIN_LEFT, self.VALUES_Y), str(income["prosperity"]), color=self.INK_COLOR,
-                        size=self.FONT_HEIGHT_TEXT)
-        self._font.text(draw, (self.MARGIN_LEFT, self.VALUES_Y), str(income["pollution"]), color=self.INK_COLOR,
-                        size=self.FONT_HEIGHT_TEXT)
+        # Production: Productivity and Pollution
+        self._font.text(draw, (self.MARGIN_LEFT + self.VALUE_MARGIN, self.VALUES_Y), f"+{income['prosperity']}",
+                        color=self.INK_COLOR, size=self.FONT_HEIGHT_VALUE)
+        self.unit_icon(card, f"+{income['prosperity']}$",
+                       (self.MARGIN_LEFT + self.VALUE_MARGIN + 45,
+                       self.VALUES_Y))
+
+        self._font.text(draw, (self.CARD_WIDTH - self.MARGIN_RIGHT - self.VALUE_MARGIN - 45, self.VALUES_Y),
+                        f"+{income['pollution']}", color=self.INK_COLOR, size=self.FONT_HEIGHT_VALUE, anchor="ra")
+        self.unit_icon(card, f"+{income['pollution']}P",
+                       (self.CARD_WIDTH - self.MARGIN_RIGHT - self.VALUE_MARGIN - self.UNIT_SIZE[0],
+                        self.VALUES_Y))
 
         if flavour:
-            self._font.text(draw, (self.MARGIN_LEFT, self.CARD_HEIGHT - self.MARGIN_BOTTOM), flavour, color=INK_COLOR,
-                            size=self.FONT_HEIGHT_FLAVOUR, wrap_width=44, anchor="ld")
+            self._font.text(draw, (self.MARGIN_LEFT, self.CARD_HEIGHT - self.MARGIN_BOTTOM), flavour,
+                            color=self.INK_COLOR, size=self.FONT_HEIGHT_FLAVOUR, wrap_width=44, anchor="ld")
 
         if show_border:
             draw.rectangle((0, 0, self.CARD_WIDTH - 1, self.CARD_HEIGHT - 1), outline=(210, 210, 210, 255))
